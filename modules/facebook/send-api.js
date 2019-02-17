6a539953012881d85feb59bea55f5c7e5a211f90
leoschet <leoschettini2@gmail.com>
2019-02-17 10:24:24 -0300

    Implement method to send synced messages

---
+++ modules/facebook/send-api.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const request = require('request');
+
+function syncText(uid, messages, index = 0) {
+
+    if (index < messages.length) {
+
+        let options = {
+            url: 'https://graph.facebook.com/v2.6/me/messages',
+            method: 'POST',
+            json: {
+                messaging_type: 'RESPONSE',
+                recipient: {
+                    id: uid
+                },
+                message: {
+                    text: messages[index]
+                }
+            },
+            qs: {
+                access_token: process.env.PAGE_ACCESS_TOKEN
+            }
+        };
+
+        // Send current message
+        request(options, function (error, response, body) {
+            if (error) {
+                // Some error occured
+                console.error('Error sending messages');
+            } else if (response.body.error) {
+                // Some error occured
+                console.error('Error: ', response.body.error);
+            } else {
+                // Send next message
+                syncText(uid, messages, index + 1);
+            }
+        });
+    } else {
+        return;
+    }
+}
+
+module.exports = {
+    syncText
+}
