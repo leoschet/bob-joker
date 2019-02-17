2f7dba68c75c0d7b1d2f310c14bd553784485ee6
leoschet <leoschettini2@gmail.com>
2019-02-17 18:51:26 -0300

    Add target_repeat as an optional

--- modules/vocabulary/actions.js
+++ modules/vocabulary/actions.js
@@ -6,17 +6,16 @@ module.exports = [
     {
         code: 'functionality',
         surfaces: ['can do', 'to_be purpose'],
-        optional: ['target_bot', 'target_chuck_norris']
+        optional: ['target_bot', 'target_chuck_norris', 'target_repeat']
     },
     {
         code: 'help',
-        surfaces: ['help', 'stuck', 'lost', 'what now'],
-        optional: ['target_sender', 'target_chuck_norris']
+        surfaces: ['help', 'stuck', 'lost', 'what now']
     },
     {
         code: 'knowledge',
         surfaces: ['know about', 'fact', 'tell something', 'tell the', 'tell about', 'what to_be', 'when to_be', 'to_be?'],
-        optional: ['target_bot', 'target_chuck_norris', 'target_comedians_club', 'info_age', 'info_height']
+        optional: ['target_bot', 'target_chuck_norris', 'target_comedians_club', 'target_repeat', 'info_age', 'info_height']
     },
     {
         code: 'tell_joke',
